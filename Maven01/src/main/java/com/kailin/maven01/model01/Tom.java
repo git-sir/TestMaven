567899196fcb6c02a881bf93328cc40e00dd4162
KailinDev-PC <369634457@qq.com>
2019-06-06 00:03:37 +0800

    Revert "练习maven常用命令：compile、test、package、clean、install，通过install命名将maven项目打包安装到本地仓库，加深对groupId和artifactId的理解，练习maven02对应maven01的依赖引用。"
    This reverts commit 5d0db5ba8d3d7c1ad757b1f471ad4c4ef91b31d1.

--- Maven01/src/main/java/com/kailin/maven01/model01/Tom.java
+++
@@ -1,7 +0,0 @@
-package com.kailin.maven01.model01;
-
-public class Tom {
-    public String sayHello(){
-        return "Hello,I am Tom";
-    }
-}
--- Maven02/src/main/java/com/kailin/maven02/model01/Robot.java
+++
@@ -1,8 +0,0 @@
-package com.kailin.maven02.model01;
-import com.kailin.maven01.model01.Tom;
-
-public class Robot {
-    public String sayHello(){
-        return "I am a Robot.I say : "+new Tom().sayHello();
-    }
-}
